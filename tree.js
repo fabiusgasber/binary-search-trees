8443bd03a580bbc2e20786bc1d33a8337222a723
Fabius Gasber <fabiusgasber@gmail.com>
2025-01-24 14:56:36 +0100

    feat: add prettyPrint() method to display the balanced binary tree

--- tree.js
+++ tree.js
@@ -6,3 +6,17 @@
         root.right = buildTree(arr.slice(mid + 1, arr.length));
         return root;
     }
+
+    const prettyPrint = (node, prefix = "", isLeft = true) => {
+        if (node === null) {
+          return;
+        }
+        if (node.right !== null) {
+          prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+        }
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+        if (node.left !== null) {
+          prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+        }
+      };
+
