3edb6686844fc77c0b4c986ce5e5c46658d0536f
Fabius Gasber <fabiusgasber@gmail.com>
2025-01-27 17:08:19 +0100

    refactor: change calculation of tree root

--- tree.js
+++ tree.js
@@ -5,7 +5,7 @@ export const Tree = (arr) => {
 
     const buildTree = (arr) => {
         if(arr.length <= 0) return null;
-        const mid = Math.floor((arr.length - 1) / 2);
+        const mid = Math.floor((arr.length) / 2);
         const root = Node(arr[mid]);
         root.left = buildTree(arr.slice(0, mid));
         root.right = buildTree(arr.slice(mid + 1, arr.length));
