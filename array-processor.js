4962f2bf0ca27f38d87bb9c9f63d80e39d314e3a
Fabius Gasber <fabiusgasber@gmail.com>
2025-01-27 10:46:59 +0100

    feat: add mergeSort algorithm to sort arrays

--- array-processor.js
+++ array-processor.js
@@ -1,5 +1,30 @@
 export const ArrayProcessor = () => {
 
+    const mergeSort = (arr) => {
+        if(arr.length <= 1) return arr;
+        const mid = Math.floor((arr.length - 1) / 2);
+        const left = mergeSort(arr.slice(0, mid + 1));
+        const right = mergeSort(arr.slice(mid + 1));
+        return merge(left, right);
+    }
+
+    const merge = (left, right) => {
+        const mergedArr = [];
+        let i = 0; 
+        let j = 0;
+        let k = 0;
+        while(i < left.length && j < right.length){
+            left[i] <= right[j] ? mergedArr[k++] = left[i++] : mergedArr[k++] = right[j++];
+        }
+        while(i < left.length){
+            mergedArr[k++] = left[i++];
+        }
+        while(j < right.length){
+            mergedArr[k++] = right[j++];
+        }
+        return mergedArr;
+    }
+
 
     return { mergeSort, removeDuplicates }
 }
